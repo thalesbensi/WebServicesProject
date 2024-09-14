bc526959cc3ca0b3c6f9d798f262d32cc671c09d
Thales Bensi <thalesbensi@outlook.com>
2024-09-13 21:59:14 -0300

    H2 Databse, Test Profile, JPA
    Dependências H2 Database e JPA  adicionadas, definido os parâmetros da entidade User para JPA, criado o file de propriedades de teste e acesso do banco H2

--- src/main/java/com/thalesbensi/WebServicesProject/entities/User.java
+++ src/main/java/com/thalesbensi/WebServicesProject/entities/User.java
@@ -3,9 +3,19 @@ package com.thalesbensi.WebServicesProject.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb-user")
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
@@ -87,8 +97,6 @@ public class User implements Serializable {
 		return serialVersionUID;
 	}
 
-
-
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -100,16 +108,4 @@ public class User implements Serializable {
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 }
